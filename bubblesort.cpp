c36d10ad6079ada3c5fc9ec3e4b8c5aef934eb48
Shifa Khan <143528975+shifakhan-11045@users.noreply.github.com>
2024-06-29 01:46:04 +0530

    Update bubblesort.cpp

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -4,7 +4,6 @@ using namespace std;
 int main()
 {
     int n,i,j,temp;
-	cout<<"Shifa Khan\n0832CS221187"<<endl;
     cout<<"Enter the size of array ";
 	cin>>n;
     cout<<"Enter the elements of array :\n";
@@ -36,7 +35,7 @@ int main()
     cout<<endl;
      for(i=0;i<n;i++)
 	{
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<"Â ";
 	}
 
 }
